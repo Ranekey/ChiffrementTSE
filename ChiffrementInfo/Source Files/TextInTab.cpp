9168b2063de374d2f77241761258b1e598e5042e
Keyrane Ndjock <keyrane.ndjock@etu.univ-st-etienne.fr>
2023-01-10 16:32:40 +0100

    TextInTab c'est terminer

--- ChiffrementInfo/Source Files/TextInTab.cpp
+++ ChiffrementInfo/Source Files/TextInTab.cpp
@@ -5,9 +5,22 @@ unsigned int TextInTab(string nomfichier, char texte[]) {
 	ifstream Fichier(nomfichier.c_str());
 	if (Fichier) {
 
+		string ligne;
+		while (getline(Fichier, ligne)) {
+			for (unsigned int i = 0; i < ligne.length(); i++) {
+				texte[ind] = ligne[i];
+				ind = ind + 1;
+			}
+			text[ind] = '\n';
+			ind = ind + 1;
+		}
 
 
 
 	}
+	else {
+		//Erreur
+	}
+	return ind //cela correspond à la taille pratique du tableau
 
 }
