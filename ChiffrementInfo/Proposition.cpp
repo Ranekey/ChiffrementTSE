892eac48ad940d4d76d04c29b7bfb2a8f2399726
Leo Viel <Leoviel@outlook.fr>
2023-01-12 12:11:16 +0100

    ajout de la fonction proposition initiale (pas finie)

--- ChiffrementInfo/Proposition.cpp
+++ ChiffrementInfo/Proposition.cpp
@@ -1,8 +1,43 @@
 #include "decrypter.h"
+#include <iostream>
+using namespace std;
 
-void Proposition_initiale(float freq[], float proposition[])
+void Trier_frequence(dicoFreq tab[])
 {
+	//Trier les deux tableaux dans l'ordre croissant en fonction de la fréquence
+	for (int i = 0; i < 26; i++)
+	{
+		dicoFreq x_temp = tab[i];
+		int j = i;
 
+		while (j > 0 && tab[j - 1].frequence > x_temp.frequence)
+		{
+			tab[j] = tab[j - 1];
+			j = j - 1;
+		}
+
+		tab[j] = x_temp;
+	}
+}
+
+void Proposition_initiale(const float freq[], char proposition[])
+{
+	dicoFreq freqBase[26];
+	dicoFreq freqMess[26];
+	char alphabet[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	dicoFreq x_temp;
+	int j;
+
+	//Associer à chaque lettre de l'alphabet sa fréquence en utiliser le type composé dicoFreq (en faire une fonction)
+	for (int i = 0; i < 26; i++)
+	{
+		freqBase[i].caractere = alphabet[i];
+		freqBase[i].frequence = freq[i];
+		freqMess[i].caractere = alphabet[i];
+		freqMess[i].frequence = proposition[i];
+	}
+	Trier_frequence(freqBase);
+	Trier_frequence(freqMess);
 }
 
 void FreqApparitions(char texte[], float freq[])
--- ChiffrementInfo/decrypter.h
+++ ChiffrementInfo/decrypter.h
@@ -1,17 +1,11 @@
-void Proposition();
-
 /*
-Rôle : Assigne un score de plausibilité à un tableau de charactère
-Entré : Un tableau de caractères étant le texte, un entier correspondant à la taille réelle du texte
-Sortie : un réel représentant le score du texte
+Type composé utilisé pour associer à chaque caractère une fréquence d'apparation (en pourcentage de 0 à 1)
 */
-float Score(char texte[], unsigned int tpTexte, float bigramme[26][26]);
-
-/*R : Etablir une proposition initial à partir d'un texte donné en fonction de la fréquence d'appartion des lettres
-E : Un tableau de la fréquence d'apparition de chacune des 26 lettres (dans l'ordre A à Z)
-E / S : Un tableau étant la proposition de l'ordre des 26 lettres
-S : vide*/
-void Proposition_initiale(float freq[], float proposition[]);
+struct dicoFreq
+{
+	char caractere;
+	float frequence;
+};
 
 /*
 R : Retourner la fréquence d'apparation des lettres en majuscules uniquement dans un texte
@@ -20,6 +14,27 @@ E / S : Un tableau de la fréquence d'apparation de chaque lettres en pourcentage
 S : vide
 */
 void FreqApparitions(char texte[], float freq[]);
+
+/*
+R : Trier dans l'ordre des fréquences d'apparitions (de 0 à 1) un tableau de type composé dicoFreq
+E : vide
+E / S : un tableau de type composé dicoFreq trier dans l'ordre décroissant des fréquences d'apparitions
+S : vide
+*/
+void Trier_frequence(dicoFreq tab[]);
+
+/*R : Etablir une proposition initial à partir d'un texte donné en fonction de la fréquence d'appartion des lettres
+E : Un tableau de la fréquence d'apparition de chacune des 26 lettres (dans l'ordre A à Z)
+E / S : Un tableau étant la proposition de l'ordre des 26 lettres
+S : vide*/
+void Proposition_initiale(const float freq[], char proposition[]);
+
+/*
+Rôle : Assigne un score de plausibilité à un tableau de charactère
+Entré : Un tableau de caractères étant le texte, un entier correspondant à la taille réelle du texte
+Sortie : un réel représentant le score du texte
+*/
+float Score(char texte[], unsigned int tpTexte, float bigramme[26][26]);
 
 /*
 Rôle: Permet de donne l'indice d'une lettre dans l'alphabet (exemple : LettreToNumber('A') = 0)
@@ -36,7 +51,6 @@ Sortie: Un nouveau texte dont on a appliquer la proposition
 */
 void ApplicationProposition(const char proposition[], char texte[], unsigned int tpTexte);
 
-
 /*
 Rôle: c'est la fonction qui permet de décrypter le message, cela se base sur un algorithme
 de reconduit(qui lui même se base sur un algorithme de métropolis)
@@ -44,5 +58,4 @@ Entrée : Une proposition donc une chaine de 26 charctère, le score actuelle du t
 l'analyse fréquentielle, définir commme une variable global ou constante )
 Sortie : un booléean, vrai si la nouvelle proposition est accepter, faux sinon
 */
-
 bool Reconduit(float scoreActuelle, const char noouvelleProposition[]);
