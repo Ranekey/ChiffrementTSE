df3bc6580b467a10b57b4c36bf8279c0acea0492
Keyrane Ndjock <keyrane.ndjock@etu.univ-st-etienne.fr>
2023-01-13 14:47:16 +0100

    metropils encore une fois

--- ChiffrementInfo/Source Files/Metropolis.cpp
+++ ChiffrementInfo/Source Files/Metropolis.cpp
@@ -3,9 +3,28 @@
 #include <math.h>
 using namespace std;
 
-bool Metropolis(float scoreActuelle, const char noouvelleProposition[], float scoreNouvelleProposition, unsigned int tailleTexte) {
+/*
+variable dans le main
+
+best_score
+best_proposition
+
+score_actuelle
+proposition_actuelle
+
+score_courant
+proposition_courante
+
+*/
+
+bool Metropolis(float best_score, char best_proposition[26], float score_actuelle, char proposition_actuelle[26], float score_courant, char proposition_courante[26], unsigned int taille) {
+	float best_score_temp = best_score;
 	float x = rand() / (float)RAND_MAX; // génére un nombre aléatoire entre 0 et 1
-	float p = exp((scoreNouvelleProposition - scoreActuelle) * tailleTexte);
+	float p = exp((score_courant - score_actuelle) * taille);
+	if (x < p) {
+		proposition_actuelle = proposition_courante;
+	}
+	
 
 
 }
