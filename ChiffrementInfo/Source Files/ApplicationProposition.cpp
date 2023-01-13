962fda702ea55b8937fe247be00acdf2d9bdc4a2
Keyrane Ndjock <keyrane.ndjock@etu.univ-st-etienne.fr>
2023-01-13 22:28:04 +0100

    c

--- ChiffrementInfo/Source Files/ApplicationProposition.cpp
+++ ChiffrementInfo/Source Files/ApplicationProposition.cpp
@@ -9,6 +9,12 @@ void ApplicationProposition(const char texte_initial[], const unsigned int tpTex
 		indLettre = LettreToNumber(proposition[indTexte]);// convertie chaque lettre en son rang dans l'alphabet
 		//Proposition est comme un dictionnaire, au rang 0 c'est la lettre A, au rang 1 la lettre B etc...
 		// la clé est l'indice (donc indLettre), la valeur est  Proposition[indLettre]
-		texte_trad[indTexte] = proposition[indLettre];
+		if (indLettre == -1) {
+			texte_trad[indTexte] = ' ';
+		}
+		else {
+			texte_trad[indTexte] = proposition[indLettre];
+		}
+		
 	}
 }
